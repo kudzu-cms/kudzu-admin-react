a992109bc15600e223b5f0414e881bbbf3b106fa
Bobby Gryzynger <bobby.gryzynger@mediacurrent.com>
2021-02-26 18:58:56 -0500

    Add decoupled login requests.

--- src/admin/login/index.js
+++ src/admin/login/index.js
@@ -1,32 +1,77 @@
+import axios from "axios";
+import qs from 'qs'
+import React from "react"
+
 import Page from "../../layout/page";
 
-const { Button, TextField } = require("@material-ui/core");
+import { Button, TextField } from "@material-ui/core";
+import { Alert } from '@material-ui/lab';
 
-function Login({baseUrl}) {
-  return (
-  <Page>
-  <h1>Login</h1>
-  <form method="post" action={`${baseUrl}/admin/login`}>
+class  Login extends React.Component {
 
-    <div style={{marginBottom: '10px'}}>
-      <TextField fullWidth label="Email address" placeholder="Enter your email address e.g. you@example.com" type="email" name="email" required={true} />
-    </div>
+  constructor(props) {
+    super(props);
+    this.state = {messages: []}
+    this.baseUrl = props.baseUrl;
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    <div style={{marginBottom: '10px'}}>
-      <TextField fullWidth label="password" placeholder="Enter your password" type="password" name="password" required={true} />
-    </div>
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+      email: event.target.email.value,
+      password: event.target.password.value
+    };
+    this.setState({messages: []});
+    axios.post(`${this.baseUrl}/admin/login`, qs.stringify(data), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+      }
+    })
+    .then((response) => {
+      this.setState({messages: []});
+      if (response.data.success === true) {
+        window.location = "/admin/content";
+      }
+    })
+    .catch((error) => {
+      let messages = this.state.messages;
+      messages.push('Login failed.')
+      this.setState({messages: messages});
+    });
 
-    <div>
-      <Button type='submit' variant='contained' size='large' color='primary' style={{marginRight: '5px'}}>
-        Log in
-      </Button>
-      <Button href={`${baseUrl}/admin/recover`} variant='contained' size='large'>
-        Forgot password?
-      </Button>
-    </div>
-  </form>
-  </Page>
-  )
+  }
+
+  render() {
+    return (
+      <Page>
+      <h1>Login</h1>
+      { this.state.messages.map(message => {
+          return <Alert severity="error" style={{marginBottom: "5px"}}>{ message }</Alert>
+        })
+      }
+      <form onSubmit={this.handleSubmit}>
+
+        <div style={{marginBottom: '10px'}}>
+          <TextField fullWidth label="Email address" placeholder="Enter your email address e.g. you@example.com" type="email" name="email" required={true} />
+        </div>
+
+        <div style={{marginBottom: '10px'}}>
+          <TextField fullWidth label="password" placeholder="Enter your password" type="password" name="password" required={true} />
+        </div>
+
+        <div>
+          <Button type='submit' variant='contained' size='large' color='primary' style={{marginRight: '5px'}}>
+            Log in
+          </Button>
+          <Button href={`${this.baseUrl}/admin/recover`} variant='contained' size='large'>
+            Forgot password?
+          </Button>
+        </div>
+      </form>
+      </Page>
+    )
+  }
 }
 
 export default Login
