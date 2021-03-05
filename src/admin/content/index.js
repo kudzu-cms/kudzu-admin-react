2c2a96ecb370a186c6ba85f5c2de7114e3caab89
Bobby Gryzynger <bobby.gryzynger@mediacurrent.com>
2021-03-05 13:46:19 -0500

    Handle content edit form.

--- src/admin/content/index.js
+++ src/admin/content/index.js
@@ -194,6 +194,31 @@ function ContentItemModify() {
   return <ContentItemDelete itemType={type} itemUuid={uuid} />
 }
 
+function handleContentEditSubmit(event, type, id, editable) {
+  event.preventDefault();
+  const formData = new FormData();
+  Object.keys(editable).map(key => {
+    if (event.target[key]) {
+      formData.append(key, event.target[key].value);
+    }
+  })
+  axios.post(`${KUDZU_BASE_URL}/api/content/update?type=${type}&id=${id}`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+    withCredentials: true,
+  })
+  .then((response) => {
+    console.log(response);
+    if (response.status === 200 && response.data.data[0].id) {
+      window.location = '/admin/content';
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+}
+
 function ContentItemEdit({itemType, itemUuid}) {
   const [itemData, dispatch] = useReducer((state, action) => { return action.payload }, {})
   useEffect(() => {
@@ -216,15 +241,33 @@ function ContentItemEdit({itemType, itemUuid}) {
   let uuid, id, slug, timestamp, updated, rest;
   ({uuid, id, slug, timestamp, updated, ...rest} = itemData);
 
+  let type = slug.slice(0, slug.indexOf('-'))
+  let externalType = type.charAt(0).toUpperCase() + type.slice(1);
+
+  let editableFields = [];
+  for (const [name, value] of Object.entries(rest)) {
+    editableFields.push([name, value]);
+  }
+
   return (
   <>
-  <form>
+  <form onSubmit={event => { handleContentEditSubmit(event, externalType, id, rest) }}>
   <Grid container spacing={3}>
     <Grid item xs={3}></Grid>
     <Grid item xs={6}>
       <Button color="primary" href="/admin/content">{'< Back'}</Button>
       <h1>{rest.title}</h1>
-      <TextField defaultValue={rest.title} fullWidth label={"Title"}>{rest.title}</TextField>
+      { editableFields.map((field, index) => {
+        let fieldName = field[0];
+        let fieldValue = field[1];
+        switch ('string') {
+          case 'string':
+            return <TextField defaultValue={fieldValue} key={`${fieldName}:${index}`} name={fieldName} fullWidth label={fieldName}/>
+          default:
+            throw new Error(`Unknown field type: ${'fieldType'}`);
+        }
+        })
+      }
     </Grid>
     <Grid item xs={3}>
       <h3>Locked fields</h3>
@@ -234,7 +277,7 @@ function ContentItemEdit({itemType, itemUuid}) {
       <TextField value={timestampFormatter.format(timestamp)} fullWidth disabled label={'Created'} />
       <TextField value={timestampFormatter.format(updated)} fullWidth disabled label={'Updated'} />
       <h3>Actions</h3>
-      <Button color="primary" variant="contained">Save</Button>&nbsp;
+      <Button color="primary" type="submit" variant="contained">Save</Button>&nbsp;
       <Button color="secondary" variant="contained" href={`/admin/content/${itemType.toLowerCase()}/${itemUuid}/delete`}>Delete</Button>
     </Grid>
   </Grid>
