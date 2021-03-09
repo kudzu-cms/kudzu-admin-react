7df1e8baf71960ec924844776f8fbe6cefe160f2
Bobby Gryzynger <bobby.gryzynger@mediacurrent.com>
2021-03-09 18:26:11 -0500

    Reduce available toolbar items.

--- src/admin/content/elements/richtext.js
+++ src/admin/content/elements/richtext.js
@@ -4,6 +4,17 @@ import { TextareaAutosize } from "@material-ui/core";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+const ckeditorConfig = {
+  toolbar: [
+    'heading',
+    'bold',
+    'italic',
+    'link',
+    'numberedList',
+    'bulletedList',
+    'blockQuote'
+  ]
+}
 
 function RichText({fieldName, fieldValue, fieldIndex}) {
   let textareaRef = null;
@@ -13,6 +24,11 @@ function RichText({fieldName, fieldValue, fieldIndex}) {
         key={`${fieldName}:richtext:${fieldIndex}`}
         editor={ ClassicEditor }
         data={fieldValue}
+        config={ckeditorConfig}
+        onReady={ editor => {
+          // Get available toolbar items. Not all are currently supported.
+          // console.log(Array.from(editor.ui.componentFactory.names()))
+        }}
         onChange={(event, editor) => {
           const data = editor.getData();
           if (textareaRef) {
@@ -33,8 +49,6 @@ function RichText({fieldName, fieldValue, fieldIndex}) {
     <TextareaAutosize key={`${fieldName}:${fieldIndex}`} name={fieldName} defaultValue={fieldValue} label={fieldName} hidden />
     </>
   )
-
-
 }
 
 export default RichText
